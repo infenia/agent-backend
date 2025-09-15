80faabc6271439d2f3acbcdcef3a957b91f51078
Ranjima07 <ranjimak07@gmail.com>
2025-09-15 20:08:39 +0530

    Flyway migration (#3)
    Indroduced Flyway migration code

--- buildSrc/src/main/kotlin/com.infenia.springboot-webflux-r2dbc-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.infenia.springboot-webflux-r2dbc-conventions.gradle.kts
@@ -18,6 +18,16 @@ plugins {
 
 dependencies {
     // Spring Data R2DBC
+
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    
+    // Database dependencies
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
+    implementation("org.postgresql:postgresql")
+    implementation("org.postgresql:r2dbc-postgresql")
+    
+
     //implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 
     // Database dependencies
@@ -25,6 +35,7 @@ dependencies {
 //    implementation("org.flywaydb:flyway-database-postgresql")
 //    implementation("org.postgresql:postgresql")
 //    implementation("org.postgresql:r2dbc-postgresql")
+
 
     // Testing database dependencies
     testImplementation("org.testcontainers:postgresql")
